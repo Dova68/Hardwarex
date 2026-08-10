bc3d2e532bced4b35b0c57a70d3e851191d12172
Santiago <ikrromm368@gmail.com>
2026-08-10 06:29:18 -0500

    AL FIN

--- mantenimiento/urls.py
+++ mantenimiento/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path("historial/", views.historial, name="historial"),
     path("perfil/", views.perfil, name="perfil"),
     path("tecnico/", views.tecnico, name="tecnico"),
+    path("equipos/", views.equipos, name="equipos"),
+    path("solicitudes/", views.solicitudes, name="solicitudes"),
+    path("mantenimientos/", views.mantenimientos, name="mantenimientos"),
+    path("reportes/", views.reportes, name="reportes"),
+    path("perfil-tecnico/", views.perfil_tecnico, name="perfil_tecnico"),
 ]
--- mantenimiento/views.py
+++ mantenimiento/views.py
@@ -1,16 +1,41 @@
 from django.shortcuts import render
 
+
 def inicio(request):
     return render(request, "index.html")
+
 
 def login(request):
     return render(request, "login.html")
 
+
 def historial(request):
     return render(request, "historial.html")
+
 
 def perfil(request):
     return render(request, "perfil.html")
 
+
 def tecnico(request):
     return render(request, "tecnico.html")
+
+
+def equipos(request):
+    return render(request, "equipos.html")
+
+
+def solicitudes(request):
+    return render(request, "solicitudes.html")
+
+
+def mantenimientos(request):
+    return render(request, "mantenimientos.html")
+
+
+def reportes(request):
+    return render(request, "reportes.html")
+
+
+def perfil_tecnico(request):
+    return render(request, "perfil_tecnico.html")
